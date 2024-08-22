7fb441b20e81b28be5f94333c8db56a47a4377fe
KIM TAEHYUN <tanggu01@connect.hku.hk>
2024-08-22 20:27:37 +0800

    first commit

---
+++ server/src/main/java/com/morganstanley/code_to_give/CodeToGiveApplication.java
@@ -0,0 +1,13 @@
+package com.morganstanley.code_to_give;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeToGiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeToGiveApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/morganstanley/code_to_give/CodeToGiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morganstanley.code_to_give;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeToGiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
