34ffbe5b6e2fccce3f5e71b5df02c3e6a0c3dde4
HYEONWOO JUNG <leojung0909@gmail.com>
2024-08-24 01:04:39 +0800

    setup error handler

---
+++ server/src/main/java/com/morganstanley/code_to_give/global/exception/ErrorCode.java
@@ -0,0 +1,21 @@
+package com.morganstanley.code_to_give.global.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+
+@Getter
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum ErrorCode {
+    EVENT_NOT_FOUND(404, "EVENT_NOT_FOUND", "Cannot Found Entity");
+
+    @Getter
+    private final int status;
+    private final String code;
+    private final String message;
+
+    ErrorCode(final int status, final String code, final String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+}
