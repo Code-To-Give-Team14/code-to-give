17ec5effea11b9668b8eb2c28a23347f30450342
LioQing <s2010157@gmail.com>
2024-08-23 21:39:24 +0800

    Add repository annotation

--- server/src/main/java/com/morganstanley/code_to_give/domain/member/entity/MemberRepository.java
+++ server/src/main/java/com/morganstanley/code_to_give/domain/member/entity/MemberRepository.java
@@ -1,7 +1,9 @@
 package com.morganstanley.code_to_give.domain.member.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Integer> {
 
 }
