bc8222da283f08b73c0620de074a0c10b1ee9cd4
LioQing <s2010157@gmail.com>
2024-08-23 17:24:14 +0800

    Add getEmbedding

--- server/src/main/java/com/morganstanley/code_to_give/ai/Recommendation.java
+++ server/src/main/java/com/morganstanley/code_to_give/ai/Recommendation.java
@@ -4,36 +4,45 @@ import com.azure.ai.openai.OpenAIClient;
 import com.azure.ai.openai.OpenAIClientBuilder;
 import com.azure.ai.openai.models.*;
 import com.azure.core.credential.AzureKeyCredential;
+import com.morganstanley.code_to_give.domain.entity.Member;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Recommendation {
-    public static Float findInterest(/* User user */) {
+
+    public static List<Float> getEmbedding(List<String> texts) {
         OpenAIClient client = new OpenAIClientBuilder()
                 .endpoint("https://openai-lio.openai.azure.com/")
                 .credential(new AzureKeyCredential(System.getenv("AZURE_API_KEY")))
                 .buildClient();
 
-        List<String> userInterests = new ArrayList<>();
-        userInterests.add("Programming");
-        userInterests.add("Computer");
+        EmbeddingsOptions embeddingsOptions = new EmbeddingsOptions(List.of(String.join(", ", texts)));
+        Embeddings embeddings = client.getEmbeddings("dev-ada", embeddingsOptions);
+
+        return embeddings.getData().get(0).getEmbedding();
+    }
+
+    public static Float findInterest(Member member) {
+        OpenAIClient client = new OpenAIClientBuilder()
+                .endpoint("https://openai-lio.openai.azure.com/")
+                .credential(new AzureKeyCredential(System.getenv("AZURE_API_KEY")))
+                .buildClient();
+
+        List<String> userInterests = new ArrayList<>(member.getInterests());
 
         List<String> eventInterests = new ArrayList<>();
         eventInterests.add("Coding");
         eventInterests.add("Hackathon");
+        List<Float> eventEmbedding = getEmbedding(eventInterests);
 
-        EmbeddingsOptions embeddingsOptions = new EmbeddingsOptions(Arrays.asList(
-                String.join(", ", userInterests),
-                String.join(", ", eventInterests)
-        ));
+        EmbeddingsOptions embeddingsOptions = new EmbeddingsOptions(List.of(String.join(", ", userInterests)));
 
         Embeddings embeddings = client.getEmbeddings("dev-ada", embeddingsOptions);
 
         return computeSimilarity(
                 embeddings.getData().get(0).getEmbedding(),
-                embeddings.getData().get(1).getEmbedding()
+                eventEmbedding
         );
     }
 
@@ -51,4 +60,5 @@ public class Recommendation {
         double dist = Math.sqrt(normASum) * Math.sqrt(normBSum);
         return (float)(dotProduct / dist);
     }
+
 }
