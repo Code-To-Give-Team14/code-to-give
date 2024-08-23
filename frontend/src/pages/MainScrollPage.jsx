16efb6ef2b3ed8536a14e6b73f6f4c3523d8e1d6
IORIshizuku <whitewind1995@gmail.com>
2024-08-23 16:35:13 +0800

    Update text and image margin, change font to Open Sans

--- frontend/src/pages/MainScrollPage.jsx
+++ frontend/src/pages/MainScrollPage.jsx
@@ -1,41 +1,50 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import '../MainScrollPage.css';
+import '../styles/MainScrollPage.css';
+import activityImage from '../assets/mainScroll_activities.jpg';
 
 const MainScrollPage = () => {
   return (
     <div className="main-scroll-page">
-      <h1>Zubin Foundation</h1>
+      <h1>The Zubin Foundation</h1>
 
       <div className="event">
         <Link to="/EventPage" style={{ display: 'contents' }}>
-          <img src="https://placehold.jp/256x200.png" alt="Latest Events" className="event-image left-image" />
+          <img src={activityImage} alt="Latest Events" className="event-image right-image" />
         </Link>
-        <div className="event-description right-description">
+        <div className="event-description left-description">
           <h2>Latest Events</h2>
           <p>This is the description for Event 1. It's going to be an amazing event where you can enjoy, learn, and connect with others. This is the description for Event 1. It's going to be an amazing event where you can enjoy, learn, and connect with others. This is the description for Event 1. It's going to be an amazing event where you can enjoy, learn, and connect with others.</p>
         </div>
       </div>
 
       <div className="event">
-        <div className="event-description left-description">
+        <div className="event-description right-description">
           <h2>Our Community</h2>
           <p>This is the description for Event 2. Don’t miss out on the opportunity to be part of something extraordinary!</p>
         </div>
         <Link to="/CommunityPage" style={{ display: 'contents' }}>
-          <img src="https://placehold.jp/256x200.png" alt="Community" className="event-image right-image" />
+          <img src="https://placehold.jp/256x200.png" alt="Community" className="event-image left-image" />
         </Link>
       </div>
 
       <div className="event">
         <Link to="/MainScrollPage" style={{ display: 'contents' }}>
-          <img src="https://placehold.jp/256x200.png" alt="Chatbot" className="event-image left-image" />
+          <img src="https://placehold.jp/256x200.png" alt="Chatbot" className="event-image right-image" />
         </Link>
-        <div className="event-description right-description">
+        <div className="event-description left-description">
           <h2>About Us</h2>
           <p>This is the description for Event 3. Join us for an unforgettable experience filled with excitement and fun.</p>
         </div>
       </div>
+      <footer>
+        <div className="footer-bottom">
+          <p>
+            &copy; 2024 The Zubin Mathani Gidumal Foundation Limited (registered charity in Hong Kong - IR 91/12344).
+            Website Developed By <a href="https://ksantechsoft.com" target="_blank" rel="noopener noreferrer">Code To Give Team 14</a>.
+          </p>
+        </div>
+      </footer>
     </div>
   );
 };
