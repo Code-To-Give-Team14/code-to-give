30e1737fe5078f1428bb45ba9ec065278d5ba599
KIM TAEHYUN <tanggu01@connect.hku.hk>
2024-08-24 01:12:44 +0800

    add get event list API for member & admin

--- server/src/main/java/com/morganstanley/code_to_give/domain/event/controller/EventController.java
+++ server/src/main/java/com/morganstanley/code_to_give/domain/event/controller/EventController.java
@@ -1,24 +1,25 @@
 package com.morganstanley.code_to_give.domain.event.controller;
 
-import com.morganstanley.code_to_give.domain.event.EventService;
 import com.morganstanley.code_to_give.domain.event.controller.request.CreateEventRequest;
 import com.morganstanley.code_to_give.domain.event.controller.response.CreateEventResponse;
+import com.morganstanley.code_to_give.domain.event.controller.response.EventListResponse;
 import com.morganstanley.code_to_give.domain.event.service.CreateEventService;
+import com.morganstanley.code_to_give.domain.event.service.GetEventListService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/events")
 public class EventController {
 
-    private final EventService eventService;
+
     private final CreateEventService createEventService;
+    private final GetEventListService getEventListService;
 
     @PostMapping
     public ResponseEntity<CreateEventResponse> createEvent(
@@ -27,6 +28,16 @@ public class EventController {
     ) {
         CreateEventResponse response = createEventService.createEvent(request);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{programId}")
+    public ResponseEntity<List<EventListResponse>> getEventList(@PathVariable Integer programId) {
+        return ResponseEntity.ok(getEventListService.getEventList(programId));
+    }
+
+    @GetMapping("/admin/{programId}")
+    public ResponseEntity<List<EventListResponse>> getEventListForAdmin(@PathVariable Integer programId) {
+        return ResponseEntity.ok(getEventListService.getEventListForAdmin(programId));
     }
 
 }
