f53a61149058d1aa1f9bf6c895490362a940b25f
HYEONWOO JUNG <leojung0909@gmail.com>
2024-08-23 16:07:44 +0800

    add entity relationship between event and training

--- server/src/main/java/com/morganstanley/code_to_give/domain/entity/Event.java
+++ server/src/main/java/com/morganstanley/code_to_give/domain/entity/Event.java
@@ -1,10 +1,13 @@
 package com.morganstanley.code_to_give.domain.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -62,5 +65,13 @@ public class Event {
     @Column(columnDefinition = "json")
     @JdbcTypeCode(SqlTypes.JSON)
     private List<String> reminder;
+
+    @OneToMany(
+        mappedBy = "event",
+        cascade = CascadeType.ALL,
+        orphanRemoval = true,
+        fetch = FetchType.LAZY
+    )
+    private List<Training> trainings;
 }
 
