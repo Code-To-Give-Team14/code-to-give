49b9593bd8f926daeaf708d0cff807ce42603631
LioQing <s2010157@gmail.com>
2024-08-23 15:17:37 +0800

    Add chatbot controller

--- server/src/main/java/com/morganstanley/code_to_give/ChatbotController.java
+++ server/src/main/java/com/morganstanley/code_to_give/ChatbotController.java
@@ -1,21 +1,23 @@
 package com.morganstanley.code_to_give;
 
-import com.morganstanley.code_to_give.chatbot.Chatbot;
+import com.morganstanley.code_to_give.ai.Chatbot;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
 public class ChatbotController {
 
     public static class ChatRequest {
-        public String message;
+        public List<Chatbot.Message> messages;
     }
 
     @PostMapping("/chat")
-    public String hello(@RequestBody ChatRequest request) {
-        return Chatbot.getResponse(request.message);
+    public Chatbot.Message chat(@RequestBody ChatRequest request) {
+        return new Chatbot.Message("bot", Chatbot.getResponse(request.messages));
     }
 }
