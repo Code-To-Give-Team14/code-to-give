47140667d0d1baf974ae26b93eaecccbf6f02db2
allywun <125728160+allywun@users.noreply.github.com>
2024-08-24 19:15:13 +0800

    Completed side bar and fixed styling issues. Added side bar to Admin Page

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Layout, Menu } from 'antd';
 import { Link } from 'react-router-dom';
+import '../styles/SideBar.css';
 
-const Sidebar = () => {
+const SideBar = () => {
     return (
-    <Router>
       <Layout>
         <Layout.Sider width={200} style={{ minHeight: '100vh' }}>
           <Menu mode="inline" defaultSelectedKeys={['1']} style={{ height: '100%', borderRight: 0 }}>
             <Menu.Item key="1">
-              <Link to="/dashboard">Dashboard</Link>
+              <Link to="/admin">Dashboard</Link>
             </Menu.Item>
             <Menu.Item key="2">
-              <Link to="/events">Events</Link>
+              <Link to="/admin/table">Events</Link>
             </Menu.Item>
           </Menu>
         </Layout.Sider>
       </Layout>
-    </Router>
     );
   };
   
-  export default Sidebar;
+  export default SideBar;
--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import SideBar from "../components/SideBar.jsx";
 
 const AdminPage = () => {
   return (
-    <div>
-      THIS IS THE ADMIN PAGE
-      <Outlet />
+    <div style={{ display: 'flex', minHeight: '100vh' }}>
+      <aside className="sidebar">
+          <button className="new-event-btn"><span>New Event</span></button>
+          <div className="categories">
+            <ul>
+            <Link className="side-link" to="/admin"><li><span>Dashboard</span></li></Link>
+            <Link className="side-link" to="/admin/table"><li><span>Events</span></li></Link>
+            </ul>
+          </div>
+        </aside>
+      <main style={{ flexGrow: 1, padding: '20px' }}>
+        <Outlet />
+      </main>
     </div>
-  )
+  );
 }
 
-export default AdminPage
+export default AdminPage;
