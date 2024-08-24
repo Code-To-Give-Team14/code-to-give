163e04b00a700d899673c346df05be1b7250c2c0
HYEONWOO JUNG <leojung0909@gmail.com>
2024-08-24 17:43:08 +0800

    publish EventCreated event to outbox table after the event created

--- server/src/main/java/com/morganstanley/code_to_give/domain/event/service/CreateEventService.java
+++ server/src/main/java/com/morganstanley/code_to_give/domain/event/service/CreateEventService.java
@@ -1,18 +1,19 @@
 package com.morganstanley.code_to_give.domain.event.service;
 
 import com.morganstanley.code_to_give.ai.Recommendation;
+import com.morganstanley.code_to_give.domain.event.EventCreatedEvent;
+import com.morganstanley.code_to_give.domain.event.EventOutboxMessageRepository;
 import com.morganstanley.code_to_give.domain.event.EventRepository;
 import com.morganstanley.code_to_give.domain.event.ProgramRepository;
 import com.morganstanley.code_to_give.domain.event.controller.request.CreateEventRequest;
 import com.morganstanley.code_to_give.domain.event.controller.response.CreateEventResponse;
 import com.morganstanley.code_to_give.domain.event.entity.Event;
+import com.morganstanley.code_to_give.domain.event.entity.EventOutboxMessage;
 import com.morganstanley.code_to_give.domain.event.entity.Program;
 import com.morganstanley.code_to_give.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 import static com.morganstanley.code_to_give.global.exception.ErrorCode.PROGRAM_NOT_FOUND;
 
@@ -22,6 +23,7 @@ public class CreateEventService {
 
     private final EventRepository eventRepository;
     private final ProgramRepository programRepository;
+    private final EventOutboxMessageRepository eventOutboxMessageRepository;
 
     @Transactional
     public CreateEventResponse createEvent(CreateEventRequest request) {
@@ -45,8 +47,18 @@ public class CreateEventService {
         );
 
         eventRepository.save(event);
+        publishEventCreatedEvent(event.getId());
 
         return new CreateEventResponse(event.getId());
+    }
+
+    public void publishEventCreatedEvent(Integer eventId) {
+        EventCreatedEvent eventCreatedEvent = new EventCreatedEvent(eventId);
+        EventOutboxMessage eventOutboxMessage = new EventOutboxMessage(
+            EventCreatedEvent.class.getSimpleName(),
+            eventCreatedEvent
+        );
+        eventOutboxMessageRepository.save(eventOutboxMessage);
     }
 
 }
